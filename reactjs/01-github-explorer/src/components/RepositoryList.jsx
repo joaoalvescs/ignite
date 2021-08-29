67ddcf793b09028fe0f478257865cb6894ffeae6
João Alves <joaoalves.cs@gmail.com>
2021-08-28 23:59:19 -0300

    Primeiro componente React

---
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,38 @@
+const repositoryName = 'unform2'
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                </li>
+                <p>Forms in React</p>
+
+                <a href="">
+                    Acessar repositórios
+                </a>
+            </ul>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                </li>
+                <p>Forms in React</p>
+
+                <a href="">
+                    Acessar repositórios
+                </a>
+            </ul>            <ul>
+                <li>
+                    <strong>unform</strong>
+                </li>
+                <p>Forms in React</p>
+
+                <a href="">
+                    Acessar repositórios
+                </a>
+            </ul>
+        </section>
+    )
+}
