4822683f25e3db82574cf0c29f9e9647fb1166f1
João Alves <joaoalves.cs@gmail.com>
2021-12-10 18:42:53 -0300

    Gerando sessão de checkout

--- reactjs/ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/ignews/src/components/SubscribeButton/index.tsx
@@ -29,3 +29,4 @@ export function SubscribeButton( { priceId }: SubscribeButtonProps) {
     )
 }
 
+
--- reactjs/ignews/src/pages/api/subscribe.ts
+++ reactjs/ignews/src/pages/api/subscribe.ts
@@ -23,6 +23,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             cancel_url: process.env.STRIPE_CANCEL_URL
         })
 
+        
         return res.status(200).json({ sessionId: stripeCheckoutSession.id })
     } else {
         res.setHeader('Allow', 'POST')
